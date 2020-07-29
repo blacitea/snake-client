70cd6d6d4751d0fdb51e6cc0906e04fd0d62019c
Angel Tsang <blacitea@gmail.com>
2020-07-29 21:11:31 +0000

    used stdin.on() to listen for input and make sure can exit with ctrl+c

--- play.js
+++ play.js
@@ -1,9 +1,25 @@
 const net = require("net"); // get the net module to work with client/server
+const { stdin } = require("process");
 const connect = require("./client").connect;
 
+const setupInput = function (input) {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  handleUserInput(input);
+  return stdin;
+};
 
-
+const handleUserInput = function (key) {
+  stdin.on('data', (key) => {
+    if (key === '\u0003') {
+      process.exit();
+    }
+    //process.stdout.write(`Move: ${key}`);
+  });
+};
 
 console.log('Connecting...');
 connect();
-
+setupInput();
