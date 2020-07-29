29855fff97decb0b8e71a4b7334d7b4c9dec88ac
Angel Tsang <blacitea@gmail.com>
2020-07-29 21:33:58 +0000

    moved setupInput to input.js, module.exports & require done

---
+++ input.js
@@ -0,0 +1,29 @@
+const stdin = process.stdin;
+
+// This let us listen for user input by checking for stdin
+const setupInput = function (input) {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  handleUserInput(input);
+  return stdin;
+};
+
+// This let us take what is inputed , and do something in response to it
+const handleUserInput = function (key) {
+  stdin.on('data', (key) => { // if a 'data' event happens, this listener catches it, and exec the callback
+  //                           //**** This is inside the callback function, with key as input ****/
+    
+    if (key === '\u0003') {       // if the input(key) is Ctrl + C, exec process.exit()
+      process.exit();
+    }
+    //process.stdout.write(`Move: ${key}`);
+  });
+};
+
+let userInput = {
+  setupInput,
+};
+
+module.exports = userInput;
