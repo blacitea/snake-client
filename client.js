fc918ca13277bd9f54cfa5aa8a929205d10af7e1
Angel Tsang <blacitea@gmail.com>
2020-07-29 22:51:01 +0000

    moved constants in it's own file

--- client.js
+++ client.js
@@ -1,17 +1,14 @@
 const net = require('net');
+const { IP, PORT } = require("./constants");
 
 // Detail of the server
-const PORT = 50541;
-const HOST = 'localhost';
-/*
-const PORT = 50541;
-const HOST = '135.23.222.131';  // use 'localhost' if you are hosting the server in the same computer
-*/
+// const PORT = 50541;
+// const HOST = 'localhost';
 
 // Get connected with the server
 const connect = function () {
   const conn = net.createConnection({
-    host: HOST, // put in the IP address of server
+    host: IP, // put in the IP address of server
     port: PORT, // put in the port of the server
   });
   // Let the computer know how to interpret incoming data (i.e. as text)
---
+++ constants.js
@@ -0,0 +1,13 @@
+// Local
+// const PORT = 50541;
+// const HOST = 'localhost';
+
+// Zoom
+const PORT = 50541;
+const IP = '135.23.222.131';
+// use 'localhost' if you are hosting the server in the same computer
+
+module.exports = {
+  PORT,
+  IP,
+};
