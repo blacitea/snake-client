88cfb3a0b0881fcae38360fe2c42bd2a800e2f33
Angel Tsang <blacitea@gmail.com>
2020-07-29 20:01:10 +0000

    barebone of connection est. can link into server

---
+++ play.js
@@ -0,0 +1,21 @@
+const net = require("net"); // get the net module to work with client/server
+
+// Detail of the server
+const PORT = 50541;
+const HOST = '135.23.222.131';  // use 'localhost' if you are hosting the server in the same computer
+
+// Get connected with the server
+const connect = function () {
+  const conn = net.createConnection({
+    host: HOST, // put in the IP address of server
+    port: PORT, // put in the port of the server
+  });
+  // Let the computer know how to interpret incoming data (i.e. as text)
+  conn.setEncoding('utf8');
+
+  return conn;
+};
+
+console.log('Connecting...');
+connect();
+
